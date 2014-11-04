6f110ffb6736a5da72cce7c99689a7ecf44b551e
Seung Eun Lee <sl2578@cornell.edu>
2014-11-04 16:27:51 +0000

    fork

--- async/warmup.ml
+++ async/warmup.ml
@@ -1,7 +1,12 @@
 open Async.Std
 
-let fork d f1 f2 =
-  failwith "My mind is aglow with whirling, transient nodes of thought..."
+let fork (d: 'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2: 'a -> 'c Deferred.t) : unit =
+	Deferred.don't_wait_for (
+		Deferred.both
+		(Deferred.bind d f1)
+		(Deferred.bind d f2); return ()
+	)
 
-let deferred_map l f =
+
+let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t) : 'b list Deferred.t =
   failwith "Ditto."

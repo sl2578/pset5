c5b81aea78a648701578c224e30be9880a8cbfd5
Seung Eun Lee <sl2578@cornell.edu>
2014-11-05 04:58:51 +0000

    merh

--- async/warmup.ml
+++ async/warmup.ml
@@ -1,11 +1,7 @@
 open Async.Std
 
 let fork (d: 'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2: 'a -> 'c Deferred.t) : unit =
-	Deferred.don't_wait_for (
-		Deferred.both
-		(Deferred.bind d f1)
-		(Deferred.bind d f2); return ()
-	)
+	ignore(d >>= (fun x -> Deferred.both))
 
 
 let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t) : 'b list Deferred.t =

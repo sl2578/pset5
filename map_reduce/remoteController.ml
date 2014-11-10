95c1f8e375c60dc73cc2e436dce2cbcb0560f335
DanielWu <dgw47@cornell.edu>
2014-11-10 16:58:23 -0500

    controller

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -1,4 +1,5 @@
 open Async.Std
+open AQueue
 
 let addr = ref []
 
@@ -15,29 +16,29 @@ module Make (Job : MapReduce.Job) = struct
      useful. Or, you can pass [~how:`Parallel] as an argument to the
      [Deferred.List] functions.*)
 
+	let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t) : 'b list Deferred.t =	
+		(* convert 'a list to 'b Deferred.t list after applying f *)
+		let list_of_deferred = List.fold_left (fun acc x -> (f x)::acc) [] l in
+			(* change each 'b Deferred.t in list to 'b *)
+			List.fold_left(fun acc d ->
+				d >>= (fun x ->
+				acc >>= (fun xs ->
+				return (x::xs))))
+				(return []) list_of_deferred
 
- (*  let reduce (k, vs) =
-    Job.reduce (k, vs) >>| fun out -> (k, out)
+  let map_reduce inputs =
+  	(* connect to each worker *)
+  	let workers = Deferred.List.map ~how:`Parallel !addr ~f:
+	  	(fun x -> match x with 
+	  		| (host, port) ->
+	  			try Tcp.connect(Tcp.to_host_and_port host port)
+	  			with InfrastructureFailure -> failwith "cannot make connection"
+	  	) in
+	  (* push each worker onto queue *)
+	  let queue = Deferred.List.fold workers (AQueue.create ())
+	  						(fun q w-> AQueue.push q w) in
 
-  module C = Combiner.Make(Job)*)
 
-  let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t) : 'b list Deferred.t =
-	(* convert 'a list to 'b deferred list *)
-	let list_of_deferred = 
-		List.fold_right (fun acc x -> (f x)::acc) l [] in 
-	(* convert 'b deferred list to 'b list differed *)
-	List.fold_right 
-	(fun x acc -> 
-		x >>= (fun h -> 
-		acc >>= (fun t -> 
-		Deferred.return (h::t)))) 
-		list_of_deferred (Deferred.return [])
-
-  let map_reduce inputs = 
-  	(deferred_map (!addr) 
-  	(match x with 
-  	| (host, port) -> try Tcp.connect(Tcp.to_host_and_port host port)) 
-  						with InfrastructureFailure -> "cannot make connection")
     (* deferred_map inputs Job.map
     >>| List.flatten
     >>| C.combine
